c7740f9158ac7f6aea1df166037a4b691168d83e
murata <murata@kmc.gr.jp>
2021-09-09 21:49:20 +0900

    tx

--- tx.go
+++ tx.go
@@ -56,13 +56,14 @@ func main() {
 	// User.Count を Transactionを使ってIncrByする
 	Measure("IncrBy User.Count (Transaction)", func() {
 		parallelUser(ctx, count, key, func() {
-			user := User{}
-			OptmisticLockPipe(ctx, key, func(tx *redis.Tx) {
+			OptmisticLockPipe(ctx, func(tx *redis.Tx, pipe redis.Pipeliner) {
+				// tx を使ってRead操作を全て行ってから
+				user := User{}
 				DecodePtrStringCmd(tx.Get(ctx, key), &user)
 				user.Count += 1
-			}, func(pipe redis.Pipeliner) {
+				// pipe を使ってWrite操作を全て行う
 				pipe.Set(ctx, key, EncodePtr(&user), 0)
-			})
+			}, key)
 		})
 	})
 
@@ -73,8 +74,8 @@ func main() {
 	Measure("IncrBy User.Count (SetNX)", func() {
 		rdbForLock.FlushDB(ctx)
 		parallelUser(ctx, count, key, func() {
-			user := User{}
 			PessimiticLock(rdb, ctx, key, func() {
+				user := User{}
 				DecodePtrStringCmd(rdb.Get(ctx, key), &user)
 				user.Count += 1
 				rdb.Set(ctx, key, EncodePtr(&user), 0)
@@ -86,8 +87,8 @@ func main() {
 	// キーが増えてしまうが、Pipelineが効くので少し速い
 	Measure("IncrBy User.Count (SetNX Pipe)", func() {
 		parallelUser(ctx, count, key, func() {
-			user := User{}
 			PessimiticLockPipe(ctx, key, func(pipe redis.Pipeliner) {
+				user := User{}
 				DecodePtrStringCmd(rdb.Get(ctx, key), &user)
 				user.Count += 1
 				pipe.Set(ctx, key, EncodePtr(&user), 0)
@@ -99,17 +100,38 @@ func main() {
 // -------------- ロック用 ------------------------------------
 
 // 楽観ロック
-func OptmisticLockPipe(ctx context.Context, key string, rf func(tx *redis.Tx), wf func(redis.Pipeliner)) {
+func OptmisticLockPipe(ctx context.Context, f func(tx *redis.Tx, pipe redis.Pipeliner), keys ...string) {
 	// errが出ず成功するまで試行する。key は複数指定可能
 	for nil != rdb.Watch(ctx, func(tx *redis.Tx) error {
-		rf(tx)
 		// ここの err が大事
 		_, err := tx.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
-			wf(pipe)
+			f(tx, pipe)
 			return nil
 		})
 		return err
-	}, key) {
+	}, keys...) {
+	}
+}
+
+// 悲観ロック用のDBを使わずPipelineを使用するもの
+func PessimiticLockPipe(ctx context.Context, key string, f func(redis.Pipeliner)) {
+	// 他のキーと競合しないようにmd5しておく(キー数が増えてしまうので注意)
+	lockHash := md5.Sum([]byte(key))
+	lockKey := hex.EncodeToString(lockHash[:])
+	for {
+		// デッドロック回避のために、時間経過でもロックが解除されるようにしている
+		if !rdb.SetNX(ctx, lockKey, "1", 5*time.Second).Val() {
+			// どのくらいSleepするかは要チューニング
+			time.Sleep(time.Microsecond * time.Duration(1000))
+			continue
+		}
+		// ２つ以上のコマンドを打つので、効率重視でPipeline使用
+		pipe := rdb.Pipeline()
+		defer pipe.Close()
+		f(pipe)
+		pipe.Del(ctx, lockKey)
+		pipe.Exec(ctx)
+		return
 	}
 }
 
@@ -132,28 +154,6 @@ func PessimiticLock(rdb *redis.Client, ctx context.Context, key string, f func()
 		}
 		f()
 		rdbForLock.Del(ctx, lockKey)
-		return
-	}
-}
-
-// 悲観ロック用のDBを使わずPipelineを使用するもの
-func PessimiticLockPipe(ctx context.Context, key string, f func(redis.Pipeliner)) {
-	// 他のキーと競合しないようにmd5しておく(キー数が増えてしまうので注意)
-	lockHash := md5.Sum([]byte(key))
-	lockKey := hex.EncodeToString(lockHash[:])
-	for {
-		// デッドロック回避のために、時間経過でもロックが解除されるようにしている
-		if !rdb.SetNX(ctx, lockKey, "1", 5*time.Second).Val() {
-			// どのくらいSleepするかは要チューニング
-			time.Sleep(time.Microsecond * time.Duration(1000))
-			continue
-		}
-		// ２つ以上のコマンドを打つので、効率重視でPipeline使用
-		pipe := rdb.Pipeline()
-		defer pipe.Close()
-		f(pipe)
-		pipe.Del(ctx, lockKey)
-		pipe.Exec(ctx)
 		return
 	}
 }
